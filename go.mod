7529ba8f1c18fbc7aff7773da9cec5f04a0deb54
Tyler <68185249+tywil04@users.noreply.github.com>
2022-12-21 00:22:27 +0000

    connect to database when server starts, load .env using godotenv

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	gorm.io/gorm v1.24.2
 )
 
+require github.com/joho/godotenv v1.4.0 // indirect
+
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- server.go
+++ server.go
@@ -5,9 +5,21 @@ import (
 
 	"PasswordServer2/api"
 	"PasswordServer2/frontend"
+
+	psDatabase "PasswordServer2/lib/database"
+	psErrors "PasswordServer2/lib/errors"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
+	database := psDatabase.DatabaseConnect()
+	if database == nil {
+		panic(psErrors.ErrorLoadingDatabase)
+	}
+
 	http.Handle("/", frontend.SvelteKitHandler("/"))
 	http.Handle("/api/v1/", api.APIHandler("/api/v1"))
 
